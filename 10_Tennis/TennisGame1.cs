9e4a105d04a1ea8d92dcc37a31fbd8c64750ff96
Ruben te Wierik <ruben.wierik@vista.com>
2023-10-13 16:05:57 +0200

    Extract methods.

--- 10_Tennis/TennisGame1.cs
+++ 10_Tennis/TennisGame1.cs
@@ -27,54 +27,81 @@ namespace TennisKata
             var tempScore = 0;
             if (_scorePlayerOne == _scorePlayerTwo)
             {
-                switch (_scorePlayerOne)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = GetScoreWhenPlayersTied();
             }
             else if (_scorePlayerOne >= 4 || _scorePlayerTwo >= 4)
             {
-                var minusResult = _scorePlayerOne - _scorePlayerTwo;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
+                score = GetScoreWhenPlayerCanWin();
             }
             else
             {
-                for (var i = 1; i < 3; i++)
+                score = GetScoreWhenMatchOngoing(score);
+            }
+            return score;
+        }
+
+        private string GetScoreWhenMatchOngoing(string score)
+        {
+            int tempScore;
+            for (var i = 1; i < 3; i++)
+            {
+                if (i == 1) tempScore = _scorePlayerOne;
+                else
                 {
-                    if (i == 1) tempScore = _scorePlayerOne;
-                    else { score += "-"; tempScore = _scorePlayerTwo; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
+                    score += "-";
+                    tempScore = _scorePlayerTwo;
+                }
+
+                switch (tempScore)
+                {
+                    case 0:
+                        score += "Love";
+                        break;
+                    case 1:
+                        score += "Fifteen";
+                        break;
+                    case 2:
+                        score += "Thirty";
+                        break;
+                    case 3:
+                        score += "Forty";
+                        break;
                 }
             }
+
+            return score;
+        }
+
+        private string GetScoreWhenPlayerCanWin()
+        {
+            string score;
+            var minusResult = _scorePlayerOne - _scorePlayerTwo;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+            return score;
+        }
+
+        private string GetScoreWhenPlayersTied()
+        {
+            string score;
+            switch (_scorePlayerOne)
+            {
+                case 0:
+                    score = "Love-All";
+                    break;
+                case 1:
+                    score = "Fifteen-All";
+                    break;
+                case 2:
+                    score = "Thirty-All";
+                    break;
+                default:
+                    score = "Deuce";
+                    break;
+            }
+
             return score;
         }
     }
